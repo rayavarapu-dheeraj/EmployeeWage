521c918f632eb2c259e4ecc5e08d1d5e1b6216c3
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-12 08:51:24 +0530

    Added : Random to check if they are present or absent

--- src/com/bridgelabz/empWage/EmpWage.java
+++ src/com/bridgelabz/empWage/EmpWage.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.empWage;
 
 public class EmpWage {
-	public static void main(String []args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Daily Wage");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee Is Present");
+		else
+			System.out.println("Employee Is Not Present");
 	}
-
 }
